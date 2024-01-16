282371f05b23d48265d25dae0c2603c27b697f5f
7alavva <halavva@gmail.com>
2024-01-16 06:49:36 +0000

    uploading sio mandate initial project
    Change-Id: I5917587f94f2683297451c2b7496a093175d4292

---
+++ webapp/controller/Create.controller.js
@@ -0,0 +1,384 @@
+sap.ui.define([
+	"sap/ui/core/library",
+	"./BaseController",
+	"sap/ui/model/json/JSONModel",
+	"../model/formatter",
+	"sap/m/library",
+	"sap/ui/core/routing/History",
+	"sap/ui/Device",
+	'sap/m/MessagePopover',
+	'sap/m/MessageItem',
+], function (coreLibrary, BaseController, JSONModel, formatter, mobileLibrary, History, Device, MessagePopover,MessageItem) {
+	"use strict";
+
+	// shortcut for sap.m.URLHelper
+	var URLHelper = mobileLibrary.URLHelper;
+	var ValueState = coreLibrary.ValueState;
+	
+	var oMessagePopover;
+		
+	return BaseController.extend("sio.hcm.mandate.controller.Create", {
+
+		formatter: formatter,
+
+		/* =========================================================== */
+		/* lifecycle methods                                           */
+		/* =========================================================== */
+
+		onInit: function () {
+			// Model used to manipulate control states. The chosen values make sure,
+			// create page is busy indication immediately so there is no break in
+			// between the busy indication for loading the view's meta data
+			
+			var oViewModel = new JSONModel({
+				busy: false,
+				delay: 0,
+				editable: true,
+				lineItemListTitle: this.getResourceBundle().getText("createLineItemTableHeading")
+				// ,
+				// to_items: [{
+				// 	Pernr: "",
+				// 	Reqno: ""
+				// }]
+			});
+			this.setModel(oViewModel, "viewModel");
+			this.initMsgPopup();
+			this.getRouter().getRoute("create").attachPatternMatched(this._onObjectMatched, this);
+		},
+		/**
+		 * Binds the view to the object path and expands the aggregated line items.
+		 * @function
+		 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
+		 * @private
+		 */
+		_onObjectMatched: function (oEvent) {
+			var that = this;
+			this.getModel("appView").setProperty("/layout", "TwoColumnsMidExpanded");
+
+			//bind context
+			var oDataModel = this.getModel();
+			// oDataModel.attachMetadataLoaded(function () {
+				that.oContext = oDataModel.createEntry("/ZI_MANDREQ_HDR", {
+					// inactive: true,
+					refreshAfterChange: true,
+					// groupId: "createGroup",
+					success: function onSuccess(oSuccess) {
+						debugger;
+					},
+					error: function onError(oError) {
+						debugger;
+					}
+				});
+
+				that.getView().setBindingContext(that.oContext);
+				
+				that.initMessagePopup();
+				
+				oDataModel.setDefaultBindingMode("TwoWay");
+			// });
+		},
+
+		/* =========================================================== */
+		/* event handlers                                              */
+		/* =========================================================== */
+
+		/**
+		 * Event handler when the share by E-Mail button has been clicked
+		 * @public
+		 */
+		onSendEmailPress: function () {
+			var oViewModel = this.getModel("viewModel");
+
+			URLHelper.triggerEmail(
+				null,
+				oViewModel.getProperty("/shareSendEmailSubject"),
+				oViewModel.getProperty("/shareSendEmailMessage")
+			);
+		},
+
+		/**
+		 * Event handler when the share in JAM button has been clicked
+		 * @public
+		 */
+		onShareInJamPress: function () {
+			var oViewModel = this.getModel("viewModel"),
+				oShareDialog = sap.ui.getCore().createComponent({
+					name: "sap.collaboration.components.fiori.sharing.dialog",
+					settings: {
+						object: {
+							id: location.href,
+							share: oViewModel.getProperty("/shareOnJamTitle")
+						}
+					}
+				});
+
+			oShareDialog.open();
+		},
+
+		/**
+		 * Updates the item count within the line item table's header
+		 * @param {object} oEvent an event containing the total number of items in the list
+		 * @private
+		 */
+		onListUpdateFinished: function (oEvent) {
+			var sTitle,
+				iTotalItems = oEvent.getParameter("total"),
+				oViewModel = this.getModel("viewModel");
+
+			// only update the counter if the length is final
+			if (this.byId("lineItemsList") &&
+				this.byId("lineItemsList").getBinding("items") &&
+				this.byId("lineItemsList").getBinding("items").isLengthFinal()) {
+				if (iTotalItems) {
+					sTitle = this.getResourceBundle().getText("createLineItemTableHeadingCount", [iTotalItems]);
+				} else {
+					//Display 'Line Items' instead of 'Line items (0)'
+					sTitle = this.getResourceBundle().getText("createLineItemTableHeading");
+				}
+				oViewModel.setProperty("/lineItemListTitle", sTitle);
+			}
+		},
+
+		/* =========================================================== */
+		/* begin: internal methods                                     */
+		/* =========================================================== */
+
+		/**
+		 * Set the full screen mode to false and navigate to master page
+		 */
+		onCloseCreatePress: function () {
+			this.getModel("appView").setProperty("/actionButtonsInfo/midColumn/fullScreen", false);
+			// No item should be selected on master after create page is closed
+			this.getOwnerComponent().oListSelector.clearMasterListSelection();
+			this.getRouter().navTo("master");
+		},
+
+		/**
+		 * Toggle between full and non full screen mode.
+		 */
+		toggleFullScreen: function () {
+			var bFullScreen = this.getModel("appView").getProperty("/actionButtonsInfo/midColumn/fullScreen");
+			this.getModel("appView").setProperty("/actionButtonsInfo/midColumn/fullScreen", !bFullScreen);
+			if (!bFullScreen) {
+				// store current layout and go full screen
+				this.getModel("appView").setProperty("/previousLayout", this.getModel("appView").getProperty("/layout"));
+				this.getModel("appView").setProperty("/layout", "MidColumnFullScreen");
+			} else {
+				// reset to previous layout
+				this.getModel("appView").setProperty("/layout", this.getModel("appView").getProperty("/previousLayout"));
+			}
+		},
+		onChagePernr: function (oEvent) {
+			debugger;
+			// this.byId("lineItemsList").getBinding("items").refresh();
+		},
+		// onAddLine: function (oEvent) {
+			
+		// 	// var oItemsTable = this.byId("lineItemsList"); // table with "rows" bound with path "ToLineItems" (navigation property of sales order)
+		//     var oItemsTable = oEvent.getSource().getParent().getParent();
+		//     var oItemsBinding = oItemsTable.getBinding("items");
+		//     // var oModel = this.getView().getModel();
+		    
+		// 	 // create transient context for subentity (sales order line item) and display it in the items table
+		// 	var newObj = {
+		// 		Pernr : ""
+		// 	};
+		// 	var oItemContext = oItemsBinding.create(newObj);
+		// 	// end-user may edit item data in a dialog
+		// 	// oCreateDialog.setBindingContext(oItemContext);
+		// },
+
+// if (!this.oEscapePreventDialog) {
+// 	this.oEscapePreventDialog = new Dialog({
+// 		title: "Dialog with prevent close",
+// 		content: new Text({ text: "Try to close this Dialog with the Escape key" }).addStyleClass("sapUiSmallMargin"),
+// 		buttons: [
+// 			new Button({
+// 				text: "Simply close",
+// 				press: function () {
+// 					this.oEscapePreventDialog.close();
+// 				}.bind(this)
+// 			})
+// 		],
+// 		escapeHandler: function (oPromise) {
+// 			if (!this.oConfirmEscapePreventDialog) {
+// 				this.oConfirmEscapePreventDialog = new Dialog({
+// 					title: "Are you sure?",
+// 					content: new Text({ text: "Your unsaved changes will be lost" }),
+// 					type: DialogType.Message,
+// 					icon: IconPool.getIconURI("message-information"),
+// 					buttons: [
+// 						new Button({
+// 							text: "Yes",
+// 							press: function () {
+// 								this.oConfirmEscapePreventDialog.close();
+// 								oPromise.resolve();
+// 							}.bind(this)
+// 						}),
+// 						new Button({
+// 							text: "No",
+// 							press: function () {
+// 								this.oConfirmEscapePreventDialog.close();
+// 								oPromise.reject();
+// 							}.bind(this)
+// 						})
+// 					]
+// 				});
+// 			}
+
+// 			this.oConfirmEscapePreventDialog.open();
+// 		}.bind(this)
+// 	});
+// }
+
+// this.oEscapePreventDialog.open();
+			// var that = this;
+			// var oDataModel = this.getModel();
+			
+			// debugger;
+			// // var to_items = oDataModel.getProperty("/to_items");
+			// var oContext = this.getView().getBindingContext();//.getProperty("/to_items");
+			// var to_items = [];
+			// if(to_items){
+			// 	var newObj = {};
+			// 	to_items.push(newObj);
+			// }
+			// oDataModel.setProperty("/to_items", to_items);
+			
+			// oDataModel.read("/Head('" + this._businessPartnerID + "')/ToSalesOrders('" + this.Reqno + "')/to_items", {
+			// 	urlParameters: {
+			// 		"$top": 1,
+			// 		"$select": "ItemPosition"
+			// 	},
+			// 	sorters: [
+			// 		new Sorter("ItemPosition", true)
+			// 	],
+			// 	success: function(oData, response) {
+			// 		var latestItemPosition = oData.results && oData.results.length > 0 ? parseInt(oData.results[0].ItemPosition, 10) + 1 : 0;
+			// 		that.oNewItemContext = oDataModel.createEntry("/SalesOrderLineItemSet", {
+			// 			properties: {
+			// 				SalesOrderID: that.Reqno, 
+			// 				DeliveryDate: new Date(),
+			// 				Quantity: "1",
+			// 				ItemPosition: "" + latestItemPosition
+			// 			}
+			// 		});
+			// 		that.onSalesOrderItemDialogOpen(true, that.oNewItemContext.getPath());
+			// 	},
+			// 	error: function(error) {
+			// 		// TODO handle the error in the correct way
+			// 	}
+			// });
+		// },
+		// onDelete: function(oEvent){
+		// 	oEvent.getParameter("listItem").getBindingContext().delete();
+		// 	// debugger;
+		// 	// to delete from BE?
+		// },
+		onAddLineLocal: function (oEvent) {
+			var newObj = {
+				Pernr: "",
+				Reqno: ""
+			};
+			var viewModel = this.getModel("viewModel");
+			var to_items = viewModel.getProperty("/to_items");
+			if (to_items) {
+				to_items.push(newObj);
+			} else {
+				to_items = [newObj];
+			}
+
+			viewModel.setProperty("/to_items", to_items);
+			viewModel.refresh(true)
+		},
+		onCancel: function (oEvent) {
+			var that = this;
+			
+			//warning dialog to confirm
+			if (!this.oApproveDialog) {
+				this.oApproveDialog = new sap.m.Dialog({
+					type: sap.m.DialogType.Message,
+					state: ValueState.Warning,
+					title: this.getResourceBundle().getText("Confirm"),
+					// content: new Text({ text: "Do you want to submit this order?" }),
+					content: new sap.m.Text({ text: this.getResourceBundle().getText("confirmLeaveMsg") }),
+					beginButton: new sap.m.Button({
+						type: sap.m.ButtonType.Emphasized,
+						text: this.getResourceBundle().getText("Confirm"),
+						press: function () {
+							// sap.m.MessageToast.show(this.getResourceBundle().getText("DraftSaved"));
+							this.oApproveDialog.close();
+							
+							// this.getModel().resetChanges();
+							that.getModel().deleteCreatedEntry(that.oContext);
+							that.getModel().refresh(true);
+							that.onNavBack();
+						}.bind(this)
+					}),
+					endButton: new sap.m.Button({
+						text: this.getResourceBundle().getText("Cancel"),
+						press: function () {
+							this.oApproveDialog.close();
+						}.bind(this)
+					})
+				});
+			}
+			
+			//that.getModel().hasPendingChanges()?
+			if(true){
+				this.oApproveDialog.open();
+			}
+			else{
+				// if no hanges just cancel without confirmation
+				
+			}
+		},
+		onSave: function (oEvent) {
+			var that = this;
+			var oModel = this.getModel();
+			var SuccessMessage = this.getResourceBundle().getText("saveSuccessMessage");
+			var ErrorMessage = this.getResourceBundle().getText("saveErrorMessage");
+
+			// this.getModel().setDeferredGroups(["createGroup"]);
+			oModel.submitChanges({
+				// groupId: "createGroup",
+				success: function onSuccess(oData, oResponse) {
+					debugger;
+					// sap.m.MessageToast.show(SuccessMessage);
+					
+					that.messageBuilder(oData);
+					
+					var Reqno = that.getView().getBindingContext().getProperty("Reqno");; //oData.Reqno
+					if (Reqno){
+						that.navtoDetail(Reqno);
+					}
+				},
+				error: function onError(oError) {
+					debugger;
+					sap.m.MessageToast.show(ErrorMessage);
+				}
+			});
+
+		},
+		navtoDetail: function (Reqno) {
+			var bReplace = !Device.system.phone;
+			// set the layout property of FCL control to show two columns
+			this.getModel("appView").setProperty("/layout", "TwoColumnsMidExpanded");
+			this.getRouter().navTo("object", {
+				objectId : Reqno
+			}, bReplace);
+		},
+		onNavBack: function () {
+			var sPreviousHash = History.getInstance().getPreviousHash(),
+				oCrossAppNavigator = sap.ushell.Container.getService("CrossApplicationNavigation");
+
+			if (sPreviousHash !== undefined || !oCrossAppNavigator.isInitialNavigation()) {
+				// eslint-disable-next-line sap-no-history-manipulation
+				history.go(-1);
+			} else {
+				this.getRouter().navTo("master", {}, true);
+			}
+		}
+	});
+
+});
---
+++ webapp/controller/Master.controller.js
@@ -0,0 +1,121 @@
+sap.ui.define([
+	"./BaseController",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/routing/History",
+	"sap/ui/model/Filter",
+	"sap/ui/model/Sorter",
+	"sap/ui/model/FilterOperator",
+	"sap/m/GroupHeaderListItem",
+	"sap/ui/Device",
+	"sap/ui/core/Fragment",
+	"../model/formatter"
+], function (BaseController, JSONModel, History, Filter, Sorter, FilterOperator, GroupHeaderListItem, Device, Fragment, formatter) {
+	"use strict";
+
+	return BaseController.extend("sio.hcm.mandate.controller.Master", {
+
+		formatter: formatter,
+
+		/* =========================================================== */
+		/* lifecycle methods                                           */
+		/* =========================================================== */
+
+		/**
+		 * Called when the master list controller is instantiated. It sets up the event handling for the master/detail communication and other lifecycle tasks.
+		 * @public
+		 */
+		onInit : function () {
+			// Control state model
+			var oSmartTable = this.byId("smartTable");
+			var	oViewModel = this._createViewModel();
+			
+			this.setModel(oViewModel, "masterView");
+
+			this.getRouter().getRoute("master").attachPatternMatched(this._onMasterMatched, this);
+		},
+		
+		/**
+		 * Event handler for the list selection event
+		 * @param {sap.ui.base.Event} oEvent the list selectionChange event
+		 * @public
+		 */
+		onSelectionChange : function (oEvent) {
+			var oSmartTable = oEvent.getSource(),
+				bSelected = oEvent.getParameter("selected");
+
+			// skip navigation when deselecting an item in multi selection mode
+			if (!(oSmartTable.getMode() === "MultiSelect" && !bSelected)) {
+				// get the list item, either from the listItem parameter or from the event's source itself (will depend on the device-dependent mode).
+				this._showDetail(oEvent.getParameter("listItem") || oEvent.getSource());
+			}
+		},
+
+		/**
+		 * Event handler for navigating back.
+		 * It there is a history entry or an previous app-to-app navigation we go one step back in the browser history
+		 * If not, it will navigate to the shell home
+		 * @public
+		 */
+		onNavBack : function() {
+			var sPreviousHash = History.getInstance().getPreviousHash(),
+				oCrossAppNavigator = sap.ushell.Container.getService("CrossApplicationNavigation");
+
+			if (sPreviousHash !== undefined || !oCrossAppNavigator.isInitialNavigation()) {
+				// eslint-disable-next-line sap-no-history-manipulation
+				history.go(-1);
+			} else {
+				oCrossAppNavigator.toExternal({
+					target: {shellHash: "#Shell-home"}
+				});
+			}
+		},
+
+		/* =========================================================== */
+		/* begin: internal methods                                     */
+		/* =========================================================== */
+		_createViewModel : function() {
+			return new JSONModel({
+				isFilterBarVisible: false,
+				filterBarLabel: "",
+				delay: 0,
+				title: this.getResourceBundle().getText("masterTitleCount", [0]),
+				noDataText: this.getResourceBundle().getText("masterListNoDataText"),
+				sortBy: "Status",
+				groupBy: "None"
+			});
+		},
+
+		_onMasterMatched :  function() {
+			//Set the layout property of the FCL control to 'OneColumn'
+			this.getModel("appView").setProperty("/layout", "OneColumn");
+		},
+
+		/**
+		 * Shows the selected item on the detail page
+		 * On phones a additional history entry is created
+		 * @param {sap.m.ObjectListItem} oItem selected Item
+		 * @private
+		 */
+		_showDetail : function (oItem) {
+			var bReplace = !Device.system.phone;
+			// set the layout property of FCL control to show two columns
+			this.getModel("appView").setProperty("/layout", "TwoColumnsMidExpanded");
+			this.getRouter().navTo("object", {
+				objectId : oItem.getBindingContext().getProperty("Reqno")
+			}, bReplace);
+		},
+		onShowDetails: function(oEvent){
+			debugger;
+		},
+		onCreate: function(oEvent){
+			// var bReplace = !Device.system.phone;
+			// set the layout property of FCL control to show two columns
+			this.getModel("appView").setProperty("/layout", "TwoColumnsMidExpanded");
+			// this.getRouter().navTo("object", {}, bReplace);
+			this.getRouter().navTo("create");
+			
+		}
+
+	});
+
+});
