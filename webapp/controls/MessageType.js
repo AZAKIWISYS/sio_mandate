1ebe9a32f3d087232c04d97d0db347ffdb61b40a
Ahmed Ayman <aayman@wisys.com.sa>
2024-01-28 11:21:30 +0000

    Library upload fixes

--- webapp/controls/MessageType.js
+++ webapp/controls/MessageType.js
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
- sap.ui.define(["sap/ui/base/DataType"], (DataType) => {
+ sap.ui.define(["./DataType"], (DataType) => {
 	"use strict";
 
 	/**
